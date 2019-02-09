5ce2b9c6b55713e21328116e65e1e89173ecc587
Addison Verger <addison.j.verger@gmail.com>
2019-02-09 12:47:18 -0600

    poker rendering complete

--- poker/index.js
+++ poker/index.js
@@ -5,5 +5,32 @@ subscribe(() => render(getState()));
 
 dispatch({ type: null }); // Here we're making a call to dispatch() - this triggers the first render.
 
-// Write DOM event listeners here, make them dispatch actions to the Redux store 
+// Write DOM event listeners here, make them dispatch actions to the Redux store
 
+const card1Button = document.getElementById("card1Button")
+card1Button.addEventListener('click', e => dispatch({
+  type: "RANDOM_CARD",
+  index: 0,
+  value: randomValue(),
+  suit: randomSuit()
+}))
+
+const card2Button = document.getElementById("card2Button")
+card2Button.addEventListener('click', e => dispatch({
+  type: "RANDOM_CARD",
+  index: 1,
+  value: randomValue(),
+  suit: randomSuit()
+}))
+
+function randomValue () {
+  valueArray = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'A', 'J', 'Q', 'K']
+  var random = valueArray[Math.floor(Math.random() * valueArray.length)];
+  return random
+}
+
+function randomSuit () {
+  valueArray = ['C', 'D', 'H', 'S']
+  var rand = valueArray[Math.floor(Math.random() * valueArray.length)];
+  return rand
+}
--- poker/reducer.js
+++ poker/reducer.js
@@ -1,19 +1,43 @@
 const initialState = [
     {
-        value: "K",
-        suit: "C"
+      value: "K",
+      suit: "C"
     },
     {
-        value: "K",
-        suit: "D"
+      value: "K",
+      suit: "D"
     }
 ];
 
 const reducer = (state = initialState, action) => {
     // Handle actions here - make sure you don't mutate the state!
     const { type } = action;
+    const newState = [...state]
 
     // ACTION - Change either card to a random new one (Try implementing one action that both buttons dispatch)
-    
+    if (type === "RANDOM_CARD") {
+
+      newState[action.index] = {
+        value: action.value,
+        suit: action.suit,
+      }
+
+      // This is inefficient //
+
+      // const newState = state.map((item, cardIndex) => {
+      //   console.log(action.index)
+      //   if (action.index === cardIndex) {
+      //     const newObject = {
+      //       value: action.value,
+      //       suit: action.suit
+      //     }
+      //     return newObject
+      //     }
+      //   return state[cardIndex]
+      // })
+      // console.log(newState)
+      
+      return newState
+    }
     return state;
 }
